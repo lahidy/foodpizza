f55ce38ea4d72c6e6250fe139e18dcbdd6a0b4c4
lahidy <16dayanarivera@gmail.com>
2017-04-03 16:43:35 -0500

    el index es la pagina principal

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :pizzas
   put "votos/:id" => "pizzas#votos", as: :pizza_votes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "pizzas#index"
 end
+
